3528375bd2975ac441709db889ba58468cbaa002
MOULIAS Mattheo <mattheo.moulias6@etu.univ-lorraine.fr>
2024-02-24 17:48:14 +0100

    Modification Total Ajout d'une timeline Version mobile adapté Version Ipad adapté

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss'
+import './css/backgroundRadiant.css'
+import { MyHeader } from "./MyHeader.jsx";
+import { HomeContent } from "./HomeContent.jsx";
+
+function App() {
+  return (
+    <div className="App">
+        <MyHeader />
+        <HomeContent />
+    </div>
+  )
+}
+
+export default App
